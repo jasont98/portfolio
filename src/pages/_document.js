855cf49649e20e805c7eea73b8d7b95253d0917b
Hoang Tran <jashoang2583@gmail.com>
2023-02-26 14:21:27 -0800

    website title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" class="bg-gray-900">
       <Head />
+      <title>JasonDevTran</title>
       <body>
         <Main />
         <NextScript />
